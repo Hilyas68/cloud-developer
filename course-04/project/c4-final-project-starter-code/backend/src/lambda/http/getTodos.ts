37b2f7683e4ee734a61b911a507c05c6e710c0ea
Hassan <htope68@gmail.com>
2023-02-19 22:05:34 +0000

    implemented auth

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -3,17 +3,44 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
+import * as AWS  from 'aws-sdk'
 
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
+//import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
 import { getUserId } from '../utils';
 
+const todosTable = process.env.TODOS_TABLE
+const index = process.env.TODOS_CREATED_AT_INDEX
+
+const docClient = new AWS.DynamoDB.DocumentClient()
 // TODO: Get all TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Write your code here
-    const todos = '...'
+    // const todos = await docClient.scan({
+    //   TableName: todosTable
+    // }).promise()
+  
+    const todos = await docClient.query({
+      TableName: todosTable,
+      IndexName: index,
+      KeyConditionExpression: 'paritionKey = :paritionKey',
+      ExpressionAttributeValues: {
+        ':paritionKey': getUserId
+      }
+}).promise()
 
-    return undefined
+    const items = todos.Items
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        items
+      })
+    }
+  })
 
 handler.use(
   cors({
